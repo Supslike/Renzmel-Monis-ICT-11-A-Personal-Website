40f52ee58fa6960f13420b6b83b46dd6f5e8d8f4
Supslike <80964103+Supslike@users.noreply.github.com>
2023-11-03 05:07:48 +0800

    Added Comments a bit

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let top_button = document.getElementById("top-button");
-let isMenuOpen = false;
-var navigations = document.querySelector('.navigations');
+let top_button = document.getElementById("top-button"); // Gets the button
+let isMenuOpen = false; // Checks if the collapsible navigator is clicked in mobile view
+var navigations = document.querySelector('.navigations'); // Gets navigation elements to later on manipulate its CSS
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()}; // Calling scrollFunction everytime a user scrolls
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    top_button.style.display = "block";
+    top_button.style.display = "block"; // reveals the button if scroll has been detected.
   } else {
-    top_button.style.display = "none";
+    top_button.style.display = "none"; // removes the button if user scrolled to the top.
   }
 }
 
 function topFunction() {
   document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0;
+  document.documentElement.scrollTop = 0; // When button is clicked, this function is called causing a scroll to the top. 
 }
 
 function toggleMenu() {
@@ -22,7 +22,7 @@ function toggleMenu() {
     if (isMenuOpen) {
         navigations.style.display = "flex";
     }   
-    navigations.style.animation = isMenuOpen ? 'nav-bar-open 0.5s ease' : 'nav-bar-close 0.5s ease';
+    navigations.style.animation = isMenuOpen ? 'nav-bar-open 0.5s ease' : 'nav-bar-close 0.5s ease'; // Adds animation of close, open of navigation
     let bar_icon = document.getElementById("bar-icon");
     bar_icon.src = isMenuOpen ? 'static/open.svg' : 'static/close.svg'
   }
@@ -32,4 +32,4 @@ navigations.addEventListener('animationend', function () {
         navigations.style.display = "none";
     }});
 
-document.getElementById('menu-toggle').addEventListener('click', toggleMenu);
+document.getElementById('menu-toggle').addEventListener('click', toggleMenu); // Activates a event listener, to wait until the user clicks the navigation bar.
